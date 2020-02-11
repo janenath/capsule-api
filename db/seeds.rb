73c93be0ba33e063deba8db837f61bb7938fc9d2
Jane Nath <janenath@Janes-MacBook-Air.local>
2020-02-10 22:13:22 -0800

    push capsule api to new repo

---
+++ db/seeds.rb
@@ -0,0 +1,130 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Item.create([
+    {
+        category: "shirts",
+        image: "https://i.imgur.com/KqYFUnL.png",
+        selected: false
+    },
+    {
+        category: "shirts",
+        image: "https://i.imgur.com/gMDFlfm.png",
+        selected: false
+    },
+    {
+        category: "shirts",
+        image: "https://i.imgur.com/qxr0RaZ.png",
+        selected: false
+    },
+    {
+        category: "shirts",
+        image: "https://i.imgur.com/e2WuvOy.png",
+        selected: false
+    },
+    {
+        category: "pants",
+        image: "https://i.imgur.com/VH5UpzC.png",
+        selected: false
+    },
+    {
+        category: "pants",
+        image: "https://i.imgur.com/IQUPabI.png",
+        selected: false
+    },
+    {
+        category: "pants",
+        image: "https://i.imgur.com/DdQiHkJ.png",
+        selected: false
+    },
+    {
+        category: "pants",
+        image: "https://i.imgur.com/KUa9pEG.png",
+        selected: false
+    },
+    {
+        category: "dresses",
+        image: "https://i.imgur.com/2Q5bgWC.png",
+        selected: false
+    },
+    {
+        category: "dresses",
+        image: "https://i.imgur.com/O5YTt5C.png",
+        selected: false
+    },
+    {
+        category: "dresses",
+        image: "https://i.imgur.com/ft8iV2M.png",
+        selected: false
+    },
+    {
+        category: "dresses",
+        image: "https://i.imgur.com/HyEsX3p.png",
+        selected: false
+    },
+    {
+        category: "jackets",
+        image: "https://i.imgur.com/trI6avF.png",
+        selected: false
+    },
+    {
+        category: "jackets",
+        image: "https://i.imgur.com/7ZdwHO2.png",
+        selected: false
+    },
+    {
+        category: "jackets",
+        image: "https://i.imgur.com/tVgKw9M.png",
+        selected: false
+    },
+    {
+        category: "jackets",
+        image: "https://i.imgur.com/jRBN5ZG.png",
+        selected: false
+    },
+    {
+        category: "accessories",
+        image: "https://i.imgur.com/QIFJ0wt.png",
+        selected: false
+    },
+    {
+        category: "accessories",
+        image: "https://i.imgur.com/N9wZVQF.png",
+        selected: false
+    },
+    {
+        category: "accessories",
+        image: "https://i.imgur.com/uEmOBSI.png",
+        selected: false
+    },
+    {
+        category: "accessories",
+        image: "https://i.imgur.com/RiK0DMe.png",
+        selected: false
+    },
+    {
+        category: "shoes",
+        image: "https://i.imgur.com/SG3wt5w.png",
+        selected: false
+    },
+    {
+        category: "shoes",
+        image: "https://i.imgur.com/6ZP79go.png",
+        selected: false
+    },
+    {
+        category: "shoes",
+        image: "https://i.imgur.com/3ruhqbG.png",
+        selected: false
+    },
+    {
+        category: "shoes", 
+        image: "https://i.imgur.com/gBJhJ7Q.png",
+        selected: false
+    }
+])
